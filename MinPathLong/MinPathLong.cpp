0abbc33d867bc7069f9c7d19d72ebe453701a024
仕旻 张 <lljkiff@163.com>
2021-01-07 22:28:12 +0800

    Upload
    迪杰斯特拉算法练习1
    初见，可爱，单挑（bushi）

--- MinPathLong/MinPathLong.cpp
+++ MinPathLong/MinPathLong.cpp
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define PointSum_Max 128
+#define PointSum_Max 8
 
 typedef struct PathLength
 {
@@ -22,7 +22,9 @@ void DeleteNode(PathLength* Node_todelete); //删除节点函数
 int TravelFind(int PointToFind);//寻找相关节点函数
 
 //定义全局变量
-int UnKnownPoint[PointSum_Max], KnownPoint[PointSum_Max];   //两个全局数组
+int* KnownPoint = NULL;     //已经确定的点
+int *UnKnownPoint = NULL;   //尚未确定的点
+int *LastRound = NULL; //上一次被查找的点
 PathLength* Head = NULL;//距离链表 头指针
 PathLength* NowNode = NULL;//当前节点
 
@@ -32,16 +34,29 @@ int main()
     
     scanf_s("%d %d %d %d", &BeginPoint, &EndPoint, &PointSum, &PointLengthSum);
 
-    //初始化已知最短路径数组
+    //分配数组内存
+    KnownPoint = (int*)malloc(PointSum * sizeof(int));
+    UnKnownPoint = (int*)malloc(PointSum * sizeof(int));
+    LastRound = (int*)malloc(PointSum * sizeof(int));
+
+    //初始化已经确定的点 为-1
     for (int i = 0; i < PointSum_Max; i++)
     {
-        KnownPoint[i] = 0; 
+        KnownPoint[i] = -1;
     }
-    //初始化未知最短路径数组
+    //初始化尚未确定的点 为65535
     for (int i = 0; i < PointSum_Max; i++)
     {
-        UnKnownPoint[i] = 1; 
+        UnKnownPoint[i] = 65535; 
     }
+    //初始化上一次被查找的点 为1
+    for (int i = 0; i < PointSum_Max; i++)
+    {
+        LastRound[i] = 1; 
+    }
+
+    UnKnownPoint[BeginPoint - 1] = 0;
+    LastRound[BeginPoint - 1] = 0;
 
     //读取点间距离
     for (int i = 0; i < PointLengthSum; i++)
@@ -49,30 +64,61 @@ int main()
         int Point1, Point2, Length;
         scanf_s("%d %d %d", &Point1, &Point2, &Length);
 
-        NowNode = NewNode(Point1, Point2, Length, NowNode);
+        NowNode = NewNode(Point1-1, Point2-1, Length, NowNode);
         if (i == 0) Head = NowNode;
     }
 
-    //记录与起点直连的点到起点的距离
-    TravelFind(BeginPoint);
 
-    int MinPoint = FindMin(KnownPoint, PointSum);//到起点距离最小的节点
+    int MinPoint = 0;//到起点距离最小的节点
     
-    TravelFind(MinPoint);
+    //遍历查找最小值
+    int ContunueFlag = 1;//是否继续搜索
+    while (ContunueFlag)
+    {
+        MinPoint = FindMin(UnKnownPoint, PointSum);
+        KnownPoint[MinPoint] = UnKnownPoint[MinPoint];//确定一个点
+
+        TravelFind(MinPoint);
+
+        ContunueFlag = 0;
+        for (int i = 0; i < PointSum; i++)
+        {
+            if (UnKnownPoint[i] == 65535)
+            {
+                ContunueFlag = 1;
+                break;
+            }
+        }
+
+        //调试信息
+        for (int i = 0; i < PointSum; i++)
+        {
+            printf("%d ", UnKnownPoint[i]);
+        }
+        printf("\n");
+    }
+
+    for (int i = 0; i < PointSum; i++)
+    {
+        printf("%d ", UnKnownPoint[i]);
+    }
+    printf("\n");
+
+    return 0;
 }
 
-//寻找最小量(上限65535)
-int FindMin(int Array[], int Sum)
+int FindMin(int* Array, int Sum)
 {
     int MinData = 0, Min = 65535;
     for (int i = 0; i < Sum; i++)
     {
-        if ((Array[i] < Min)&&(Array[i] != -1))
+        if ((UnKnownPoint[i] < Min)&&(KnownPoint[i] == -1))
         {
             MinData = i;
-            Min = Array[i];
+            Min = UnKnownPoint[i];
         }
     }
+
     return MinData;
 }
 
@@ -101,11 +147,19 @@ void DeleteNode(PathLength* Node_todelete)	//删除节点函数
     PathLength* nextNode = Node_todelete->next;		//找到下一节点
 
     //进行索引
-    beforeNode->next = nextNode;
-    nextNode->before = beforeNode;
+    if (beforeNode)
+    {
+        beforeNode->next = nextNode;
+    }
+    else {
+        Head = nextNode;
+    }
+    if (nextNode) nextNode->before = beforeNode;
 
     //删除节点释放内存
     free(Node_todelete);
+
+    Node_todelete = nextNode;
 
     return;
 }
@@ -115,20 +169,32 @@ int TravelFind(int PointToFind)//寻找相关节点函数
     NowNode = Head;//将当前指针移至链表头
     while (NowNode)//开始遍历
     {
-        if (NowNode->point1 == PointToFind)
+        if ((NowNode->point1 == PointToFind)&&(KnownPoint[NowNode->point2] == -1))
         {
-            KnownPoint[NowNode->point2] = NowNode->length;
-            UnKnownPoint[NowNode->point2] = 0;
+            if (UnKnownPoint[PointToFind] + NowNode->length < UnKnownPoint[NowNode->point2])
+            {
+                UnKnownPoint[NowNode->point2] = UnKnownPoint[PointToFind] + NowNode->length;
+            }
+            
+            PathLength* temporary = NowNode->next;//临时寄存变量
             DeleteNode(NowNode);
+            NowNode = temporary;//下一数据
             continue;
         }
-        if (NowNode->point2 == PointToFind)
+        if ((NowNode->point2 == PointToFind) && (KnownPoint[NowNode->point1] == -1))
         {
-            KnownPoint[NowNode->point1] = NowNode->length;
-            UnKnownPoint[NowNode->point1] = 0;
+            if (UnKnownPoint[PointToFind] + NowNode->length < UnKnownPoint[NowNode->point1])
+            {
+                UnKnownPoint[NowNode->point1] = UnKnownPoint[PointToFind] + NowNode->length;
+            }
+            
+            PathLength* temporary = NowNode->next;//临时寄存变量
             DeleteNode(NowNode);
+            NowNode = temporary;//下一数据
             continue;
         }
         NowNode = NowNode->next;//下一数据
     }
+
+    return 0;
 }
